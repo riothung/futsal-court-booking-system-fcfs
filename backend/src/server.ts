04cebe1e5ba19f7a68da9ef5ebfcddb0a5d366ee
Rio Thung <mario.thung@gmail.com>
2026-02-11 19:04:28 +0700

    refactor: change title, refactor some code in server to make it more simple

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,10 +3,6 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
-});
-
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on PORT:${port}`);
 });
