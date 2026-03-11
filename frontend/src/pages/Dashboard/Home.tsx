d44c26985c056a6d6ac80b534ea750b4e51438be
riothung <mario.thung@gmail.com>
2026-03-11 22:05:32 +0800

    fix: remove unwanted feature and add futsal booking card in home

--- frontend/src/pages/Dashboard/Home.tsx
+++ frontend/src/pages/Dashboard/Home.tsx
@@ -1,40 +1,31 @@
-import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
-import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
-import StatisticsChart from "../../components/ecommerce/StatisticsChart";
-import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
-import RecentOrders from "../../components/ecommerce/RecentOrders";
-import DemographicCard from "../../components/ecommerce/DemographicCard";
-import PageMeta from "../../components/common/PageMeta";
+// import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
+// import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
+// import StatisticsChart from "../../components/ecommerce/StatisticsChart";
+// import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
+// import RecentOrders from "../../components/ecommerce/RecentOrders";
+// import DemographicCard from "../../components/ecommerce/DemographicCard";
+// import PageMeta from "../../components/common/PageMeta";
+import { useState } from "react";
+import { FutsalBookingCard } from "../../components/cards/FutsalBookingCard";
+import { BookingModal } from "../../components/modals/BookingModal";
+import lapanganTanahMerah from "../../assets/images/lapangan-tanah-merah.jpg";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedCourt, setSelectedCourt] = useState<string | null>(null);
+
+  const pricePerHour = 175000;
+
+  const handleOpen = (courtName: string) => {
+    setSelectedCourt(courtName);
+    setIsOpen(true);
+  };
+
   return (
     <>
-      <PageMeta
-        title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
-        description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
-      />
-      <div className="grid grid-cols-12 gap-4 md:gap-6">
-        <div className="col-span-12 space-y-6 xl:col-span-7">
-          <EcommerceMetrics />
-
-          <MonthlySalesChart />
-        </div>
-
-        <div className="col-span-12 xl:col-span-5">
-          <MonthlyTarget />
-        </div>
-
-        <div className="col-span-12">
-          <StatisticsChart />
-        </div>
-
-        <div className="col-span-12 xl:col-span-5">
-          <DemographicCard />
-        </div>
-
-        <div className="col-span-12 xl:col-span-7">
-          <RecentOrders />
-        </div>
+      <div className="max-w-5xl mx-auto p-6">
+        <FutsalBookingCard image={lapanganTanahMerah} title="Lapangan Tanah Merah" price={175000} onBook={handleOpen}></FutsalBookingCard>
+        {selectedCourt && <BookingModal isOpen={isOpen} onClose={() => setIsOpen(false)} courtName={selectedCourt} pricePerHour={pricePerHour}></BookingModal>}
       </div>
     </>
   );
